a244e64ba7c956022f378b36a522a3af3431f667
Rehan Haider <email@rehanhaider.com>
2025-05-29 15:20:57 +0000

    FEAT:  no turning back

--- app/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@env" {
-    export const AWS_REGION: string;
-    export const AWS_USER_POOL_ID: string;
-    export const AWS_USER_POOL_CLIENT_ID: string;
-    export const AWS_IDENTITY_POOL_ID: string;
-    export const API_URL: string;
-    export const S3_BUCKET: string;
-}
